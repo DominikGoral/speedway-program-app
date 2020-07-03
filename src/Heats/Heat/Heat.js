c725131b8219089a458a70c24d46709f31fb03ef
Dominik Góral <dominik.goral@onet.pl>
2020-07-03 02:24:36 +0200

    Carousel almost done, but still few things to improve

---
+++ src/Heats/Heat/Heat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Heat.css'
+
+const heat = props => {
+    return (
+        <div className="HeatBox">
+
+        </div>
+    )
+}
+
+export default heat
--- src/Heats/Heats.js
+++ src/Heats/Heats.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import Heat from './Heat/Heat'
 import './Heats.css'
+import { render } from '@testing-library/react'
 
 const heats = props => {
-    return (
-        <div className="HeatsBox">
-
-        </div>
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToSHow: 1,
+        slidesToScroll: 1
+    }
+    return(
+        <Slider {...settings}>
+            <div>
+                <Heat />
+            </div>
+            <div>
+                <Heat />
+            </div>
+            <div>
+                <Heat />
+            </div>
+        </Slider>
     )
 }
 
